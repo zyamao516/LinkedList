5d0554703cf80cccad6b7edd30af4f035b12b7e1
zyamao516 <104101250+zyamao516@users.noreply.github.com>
2024-02-24 20:52:23 -0800

    Updated LinkedList.h
    comments need touching up

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -10,8 +10,8 @@ namespace linkedList {
 	class LinkedList {
 
 		// Private variables head and tail pointers
-		std::shared_ptr<node::Node<T>> Head = nullptr;
-		std::shared_ptr<node::Node<T>> Tail = nullptr;
+		std::unique_ptr<node::Node<T>> Head = nullptr;
+		node::Node<T>* Tail = nullptr;
 		// number of elements in the list
 		int count = 0;
 
@@ -19,12 +19,15 @@ namespace linkedList {
 		// Input:		reference type T Element
 		// Output:		shared pointer to new Node with Element
 		// Function:	Creates new node and returns a shared pointer to it
-		inline std::shared_ptr<node::Node<T>> newElement(T& Element) {
-			auto newNode = std::make_shared<node::Node<T>>(Element);
-			return newNode;
+		inline std::unique_ptr<node::Node<T>> newElement(T& Element) {
+			auto newNode = std::make_unique<node::Node<T>>(Element);
+			return std::move(newNode);
 		}
-		inline std::shared_ptr<const node::Node<T>> getHead() const noexcept { return Head; }
-		inline std::shared_ptr<const node::Node<T>> getTail() const noexcept { return Tail; }
+
+		inline void addEmpty(std::unique_ptr<node::Node<T>>&& newNode) { Head = std::move(newNode); Tail = Head.get(); }
+
+		inline node::Node<T>* getHead() const noexcept { return Head.get(); }
+		inline node::Node<T>* getTail() const noexcept { return Tail; }
 	public:
 
 		// Default constructor, defaults to initialised values of private variables
@@ -39,30 +42,16 @@ namespace linkedList {
 
 		// copy constructor, takes in a linked list of same type and copies nodes in linked list
 		// Pointer copy creates shallow copy of list, Use with caution
-		explicit LinkedList(const LinkedList<T>& referenceList, bool pointerCopy = false) {
-			std::shared_ptr<node::Node<T>> iterator = referenceList.getHead();
-			if (pointerCopy) {
-				this->Head = referenceList.getHead();
-				this->Tail = referenceList.getTail();
-				this->count = referenceList.getcount();
-				return;
-			}
+		explicit LinkedList(const LinkedList<T>& referenceList) {
+			node::Node<T>* iterator = referenceList.getHead();
 			while (iterator != nullptr) {
-				this->addTail(iterator->getElement());
-				this->count++;
+				addTail(iterator->getElement());
+				count++;
 				iterator = iterator->Next;
 			}
 		}
 
-		~LinkedList() {
-			if (!Head) { return; }
-			std::shared_ptr<node::Node<T>> iterator = Head;
-			for (int i = 0; i < count; i++) {
-				iterator = iterator->Next;
-				iterator->Prev.reset();
-			}
-			iterator.reset();
-		}
+		~LinkedList() { Head.reset(); }
 
 		// Utility functions
 
@@ -72,16 +61,16 @@ namespace linkedList {
 		// Function:	adds new element of type T to head of the linked list
 		void addHead(T& Element) {
 			// new element created
-			auto newNode = newElement(Element);
+			std::unique_ptr<node::Node<T>> newNode = newElement(Element);
 			// checks if list is empty
 			if (!Head) {
-				Head = Tail = newNode;
+				addEmpty(std::move(newNode));
 				return;
 			}
 			// sets pointers
-			newNode->Next = Head;
-			Head->Prev = newNode;
-			Head = newNode;
+			Head->Prev = newNode.get();
+			newNode->Next = std::move(Head);
+			Head = std::move(newNode);
 			count++;
 		}
 
@@ -91,16 +80,16 @@ namespace linkedList {
 		// Function:	adds new element of type T to tail of the linked list
 		void addTail(T& Element) {
 			// new element created
-			auto newNode = newElement(Element);
+			std::unique_ptr<node::Node<T>> newNode = newElement(Element);
 			// checks if list is empty
 			if (!Head) {
-				Head = Tail = newNode;
+				addEmpty(std::move(newNode));
 				return;
 			}
 			// sets pointers
-			newNode->Next = nullptr;
-			Tail->Prev = newNode;
-			Tail = newNode;
+			newNode->Prev = Tail;
+			Tail->Next = std::move(newNode);
+			Tail = Tail->Next.get();
 			count++;
 		}
 
@@ -109,21 +98,33 @@ namespace linkedList {
 		// Output:		void
 		// Function:	adds new element of type T to index of the linked list
 		void add(T& Element, int index) {
-			if (index >= count || index < 0) { return; }
-			count++;
-			if (index == 0) { addHead(Element); return; }
-			if (index == count - 1) { addTail(Element); return; }
-			std::shared_ptr<node::Node<T>> iterator = getHead();
-			auto newNode = newElement(Element);
-			for (int i = 0; i < index; i++) {
-				iterator = iterator->Next;
+			if (index > count || index < 0) { return; } // Check for valid index
+			if (index == 0) {
+				addHead(Element); // Add at the head if index is 0
+				return;
 			}
-			newNode->Next = iterator;
-			newNode->Prev = iterator->Prev;
-			newNode->Next->Prev = newNode;
-			newNode->Prev->Next = newNode;
+			else if (index == count) {
+				addTail(Element); // Add at the tail if index is equal to count
+				return;
+			}
 
+			std::unique_ptr<node::Node<T>> newNode = newElement(Element); // Create the new node
+			node::Node<T>* iterator = getHead();
+			for (int i = 0; i < index - 1; ++i) { // Move to the node just before the index
+				iterator = iterator->Next.get();
+			}
+
+			// Inserting newNode in between nodes
+			newNode->Next = std::move(iterator->Next); // Set newNode's next to iterator's next
+			if (newNode->Next) {
+				newNode->Next->Prev = newNode.get(); // Set newNode as the previous of its next node
+			}
+			newNode->Prev = iterator; // Set newNode's prev to iterator
+			iterator->Next = std::move(newNode); // Set iterator's next to newNode
+
+			count++; // Increment count after insertion
 		}
+
 
 		// Removes element at head of list
 		// Input:		None
@@ -132,9 +133,8 @@ namespace linkedList {
 		void removeHead() {
 			if (!Head) { return; } // Returns if list is empty
 			if (count == 1) { Tail = nullptr; } // sets tail to NULL if only 1 element in list
-			std::shared_ptr<node::Node<T>> temp = Head;
-			Head = Head->Next;
-			temp.reset();
+			else { Head->Next->Prev = nullptr; }
+			Head = std::move(Head->Next);
 			count--;
 		}
 
@@ -143,11 +143,15 @@ namespace linkedList {
 		// Output:		void
 		// Function:	Removes element from tail of list
 		void removeTail() {
-			if (count == 0) { return; } // Returns if list is empty
-			if (count == 1) { Head = nullptr; } // Sets tail to NULL if only 1 element in list
-			std::shared_ptr<node::Node<T>> temp = Tail;
+			if (!Head) { return; } // Returns if list is empty
+			if (count == 1) {
+				Head.reset();
+				Tail = nullptr;
+				count--;
+				return;
+			}
 			Tail = Tail->Prev;
-			temp.reset();
+			Tail->Next.reset();
 			count--;
 		}
 
@@ -157,17 +161,15 @@ namespace linkedList {
 		// Function:	Removes element from index of list
 		void remove(int index) {
 			if (index >= count || index < 0) { return; }
-			count--;
 			if (index == 0) { removeHead(); return; }
-			if (index == count - 1) { addTail(); return; }
-			std::shared_ptr<node::Node<T>> iterator = getHead();
-			for (int i = 0; i < index; i++) {
-				iterator = iterator->Next;
+			if (index == count - 1) { removeTail(); return; }
+			node::Node<T>* iterator = getHead();
+			for (int i = 0; i < index - 1; i++) {
+				iterator = iterator->Next.get();
 			}
-			iterator->Next->Prev = iterator->Prev;
-			iterator->Prev->Next = iterator->Next;
-			iterator.reset();
-
+			iterator->Next = std::move(iterator->Next->Next);
+			iterator->Next->Prev = iterator;
+			count--;
 		}
 
 		// check if element is in list
@@ -176,7 +178,7 @@ namespace linkedList {
 		// Function:	Search list to see if element is in the array, if found returns true
 		bool find(const T& searchElement) {
 			if (isEmpty()) { return false; }
-			std::shared_ptr<node::Node<T>> iterator = getHead();
+			node::Node<T>* iterator = getHead();
 			while (iterator != nullptr) {
 				if (iterator->getElement() == searchElement) { return true; }
 				iterator = iterator->Next;
@@ -189,11 +191,11 @@ namespace linkedList {
 		// Input:		index to retrive from
 		// Output:		Copy of element at index
 		// Function:	Traverses list untill it reaches index, then returns value at index
-		T getElement(int index) const {
-			if (isEmpty() || index >= count || index < 0) { return nullptr; }
-			std::shared_ptr<node::Node<T>> iterator = getHead();
+		T& getElement(int index) const {
+			if (isEmpty() || index >= count || index < 0) { throw std::out_of_range("Index is out of range"); }
+			node::Node<T>* iterator = getHead();
 			for (int i = 0; i < index; i++) {
-				iterator = iterator->Next;
+				iterator = iterator->Next.get();
 			}
 			return iterator->getElement();
 		}
@@ -204,10 +206,8 @@ namespace linkedList {
 		// Function:	Traverses list untill it reaches index, then replaces value at index
 		void setElement(T& newElement, int index) {
 			if (isEmpty() || index >= count || index < 0) { return; }
-			std::shared_ptr<node::Node<T>> iterator = getHead();
-			for (int i = 0; i < index; i++) {
-				iterator->getElement();
-			}
+			node::Node<T>* iterator = getHead();
+			for (int i = 0; i < index; i++) { iterator = iterator->Next.get(); }
 			iterator->setElement(newElement);
 		}
 
